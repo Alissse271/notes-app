1499d30eff0c9c46bc4df941dac7411609d541fb
Alissse271 <alice_2700@mail.ru>
2023-03-21 20:14:06 +0300

    feat(ui): add style to tags list

--- src/components/TagsList/styles.ts
+++ src/components/TagsList/styles.ts
@@ -6,6 +6,7 @@ const StyledTagsList = styled.ul`
     gap: 10px;
 
     padding: 20px;
+    margin-bottom: 40px;
 
     border: 1px solid rgb(169, 169, 169);
     border-radius: 10px;
--- src/templates/MainTemplate/MainTemplate.tsx
+++ src/templates/MainTemplate/MainTemplate.tsx
@@ -52,7 +52,7 @@ export const MainTemplate = () => {
             )}
 
             <StyledButton
-                type={'button'}
+                type="button"
                 label="Delete all notes"
                 onClick={handleDeleteAllNotes}
             />
